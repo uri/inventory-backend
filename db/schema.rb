1eb0c0fbe0567d330a8abd6cfd88e8550079ecfd
Anton Sakovich <sakovias@gmail.com>
2014-06-28 15:02:42 -0400

    Generated items model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140628173329) do
+ActiveRecord::Schema.define(version: 20140628190037) do
+
+  create_table "items", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
