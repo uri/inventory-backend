74516c7d0058d2ed4fc2c88d577ac82d89450fdc
uri <uri.gore@gmail.com>
2014-06-28 15:39:29 -0400

    Added bootstrap

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.paths << Rails.root.join("lib", "angular")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "components")
+Rails.application.config.assets.precompile += %w( angular/angular-strap.tpl.min.js )
