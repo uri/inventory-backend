8e3d715a94f4378268f767e0d5b5aba9c85eabc0
uri <uri.gore@gmail.com>
2014-06-28 14:10:58 -0400

    Added angular libraries.

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Artengine
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << Rails.root.join("lib", "angular")
+    config.assets.paths << Rails.root.join("vendor", "components")
   end
 end
